5cf4cb8b8184534152bb2ebb92e13766d60773e0
jschendel <Jeremy.R.Schendel@gmail.com>
2013-12-11 20:41:00 -0600

    Eulerian Cycle Problem (updated)
    Updated with my own faster running algorithm.  Also, wrote an Eulerian
    Cycle function for future use.  The NetworkX solution is included and
    labeled as such.

--- Assignment_04E.py
+++ Assignment_04E.py
@@ -10,30 +10,64 @@ Problem ID: E
 URL: https://beta.stepic.org/Bioinformatics-Algorithms-2/From-Eulers-Theorem-to-an-Algorithm-for-Finding-Eulerian-Cycles-203/#step-2
 '''
 
-import networkx as nx
 
-# Read the input data.
-with open('data/stepic_4e.txt') as input_data:
-    edges = [line.strip().split(' -> ') for line in input_data.readlines()]
+def eulerian_cycle(edge_dict):
+    '''Generates an Eulerian cycle from the given edges.'''
+    current_node = edge_dict.keys()[0]
+    path = [current_node]
 
-# Properly format the edges.
-edges2 = []
-for edge in edges:
-    if ',' in edge[1]:
-        for node in edge[1].split(','):
-            edges2.append(map(int, [edge[0], node]))
-    else:
-        edges2.append(map(int, edge))
+    # Get the initial cycle.
+    while True:
+        path.append(edge_dict[current_node][0])
 
-# Create the graph.
-G = nx.DiGraph()
-G.add_edges_from(edges2)
+        if len(edge_dict[current_node]) == 1:
+            del edge_dict[current_node]
+        else:
+            edge_dict[current_node] = edge_dict[current_node][1:]
 
-# Find an eulerian cycle.
-path = [str(e[0]) for e in nx.eulerian_circuit(G)]
-path.append(path[0])
+        if path[-1] in edge_dict:
+            current_node = path[-1]
+        else:
+            break
 
-# Print and save the answer.
-print '->'.join(path)
-with open('output/Assignment_04E.txt', 'w') as output_data:
-    output_data.write('->'.join(path))
+    # Continually expand the initial cycle until we're out of edge_dict.
+    while len(edge_dict) > 0:
+        for i in xrange(len(path)):
+            if path[i] in edge_dict:
+                current_node = path[i]
+                cycle = [current_node]
+                while True:
+                    cycle.append(edge_dict[current_node][0])
+
+                    if len(edge_dict[current_node]) == 1:
+                        del edge_dict[current_node]
+                    else:
+                        edge_dict[current_node] = edge_dict[current_node][1:]
+
+                    if cycle[-1] in edge_dict:
+                        current_node = cycle[-1]
+                    else:
+                        break
+
+                path = path[:i] + cycle + path[i+1:]
+                break
+    return path
+
+if __name__ == '__main__':
+
+    # Read the input data.
+    with open('data/stepic_4e.txt') as input_data:
+        edges = {}
+        for edge in [line.strip().split(' -> ') for line in input_data.readlines()]:
+            if ',' in edge[1]:
+                edges[int(edge[0])] = map(int,edge[1].split(','))
+            else:
+                edges[int(edge[0])] = [int(edge[1])]
+
+    # Get the Eulerian cycle.
+    path = eulerian_cycle(edges)
+
+    # Print and save the answer.
+    print '->'.join(map(str,path))
+    with open('output/Assignment_04E.txt', 'w') as output_data:
+        output_data.write('->'.join(map(str,path)))
