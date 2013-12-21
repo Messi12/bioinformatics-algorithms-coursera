957f05b0f4ab4a1a2685125396bbb0e004d22a98
jschendel <Jeremy.R.Schendel@gmail.com>
2013-12-21 16:28:23 -0600

    Global Alignment
    Solution to Bioinformatics Algorithms Problem 6E: Global Alignment

---
+++ Assignment_06E.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+'''
+A solution to a programming assignment for the Bioinformatics Algorithms (Part 1) on Coursera.
+The associated textbook is Bioinformatics Algorithms: An Active-Learning Approach by Phillip Compeau & Pavel Pevzner.
+The course is run on Coursera and the assignments and textbook are hosted on Stepic
+
+Problem Title: Global Alignment
+Assignment #: 06
+Problem ID: E
+URL: https://beta.stepic.org/Bioinformatics-Algorithms-2/From-Global-to-Local-Alignment-247/#step-3
+'''
+
+from scripts import BLOSUM62
+
+
+def global_alignment(v, w, scoring_matrix, sigma):
+    from numpy import unravel_index, zeros
+
+    # Initialize the matrices.
+    S = zeros((len(v)+1, len(w)+1), dtype=int)
+    backtrack = zeros((len(v)+1, len(w)+1), dtype=int)
+
+    # Initialize the edges with the given penalties.
+    for i in xrange(1, len(v)+1):
+        S[i][0] = -i*sigma
+    for j in xrange(1, len(w)+1):
+        S[0][j] = -j*sigma
+
+    # Fill in the Score and Backtrack matrices.
+    for i in xrange(1, len(v)+1):
+        for j in xrange(1, len(w)+1):
+            scores = [S[i-1][j] - sigma, S[i][j-1] - sigma, S[i-1][j-1] + scoring_matrix[v[i-1], w[j-1]]]
+            S[i][j] = max(scores)
+            backtrack[i][j] = scores.index(S[i][j])
+
+    # Quick lambda function to insert indels.
+    insert_indel = lambda word, i: word[:i] + '-' + word[i:]
+
+    # Initialize the aligned strings as the input strings.
+    v_aligned, w_aligned = v, w
+
+    # Get the position of the highest scoring cell in the matrix and the high score.
+    i,j = unravel_index(S.argmax(), S.shape)
+    max_score = str(S[i][j])
+
+    # Backtrack to the edge of the matrix starting at the highest scoring cell.
+    while i*j != 0:
+        if backtrack[i][j] == 0:
+            i -= 1
+            w_aligned = insert_indel(w_aligned, j)
+        elif backtrack[i][j] == 1:
+            j -= 1
+            v_aligned = insert_indel(v_aligned, i)
+        else:
+            i -= 1
+            j -= 1
+
+    # Prepend the necessary preceeding indels to get to (0,0).
+    for repeat in xrange(i):
+        w_aligned = insert_indel(w_aligned, 0)
+    for repeat in xrange(j):
+        v_aligned = insert_indel(v_aligned, 0)
+
+    return max_score, v_aligned, w_aligned
+
+if __name__ == '__main__':
+
+    # Read the input data.
+    with open('data/stepic_6e.txt') as input_data:
+        word1, word2 = [line.strip() for line in input_data.readlines()]
+
+    # Get the alignment.
+    alignment = global_alignment(word1, word2, BLOSUM62(), 5)
+
+    # Print and save the answer.
+    print '\n'.join(alignment)
+    with open('output/Assignment_06E.txt', 'w') as output_data:
+        output_data.write('\n'.join(alignment))
