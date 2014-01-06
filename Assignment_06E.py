1dd072942cb412c6f909408895eb894560576996
jschendel <Jeremy.R.Schendel@gmail.com>
2014-01-06 00:16:20 -0600

    Global Alignment (updated)
    Fixed a minor bug sometimes causing an incorrect answer.  Switched from
    numpy arrays to lists for a speed increase.

--- Assignment_06E.py
+++ Assignment_06E.py
@@ -10,15 +10,12 @@ Problem ID: E
 URL: https://beta.stepic.org/Bioinformatics-Algorithms-2/From-Global-to-Local-Alignment-247/#step-3
 '''
 
-from scripts import BLOSUM62
-
 
 def global_alignment(v, w, scoring_matrix, sigma):
-    from numpy import unravel_index, zeros
 
     # Initialize the matrices.
-    S = zeros((len(v)+1, len(w)+1), dtype=int)
-    backtrack = zeros((len(v)+1, len(w)+1), dtype=int)
+    S = [[0 for repeat_j in xrange(len(w)+1)] for repeat_i in xrange(len(v)+1)]
+    backtrack = [[0 for repeat_j in xrange(len(w)+1)] for repeat_i in xrange(len(v)+1)]
 
     # Initialize the edges with the given penalties.
     for i in xrange(1, len(v)+1):
@@ -40,7 +37,7 @@ def global_alignment(v, w, scoring_matrix, sigma):
     v_aligned, w_aligned = v, w
 
     # Get the position of the highest scoring cell in the matrix and the high score.
-    i,j = unravel_index(S.argmax(), S.shape)
+    i, j = len(v), len(w)
     max_score = str(S[i][j])
 
     # Backtrack to the edge of the matrix starting at the highest scoring cell.
@@ -64,15 +61,19 @@ def global_alignment(v, w, scoring_matrix, sigma):
     return max_score, v_aligned, w_aligned
 
 if __name__ == '__main__':
+    from scripts import BLOSUM62
 
     # Read the input data.
     with open('data/stepic_6e.txt') as input_data:
         word1, word2 = [line.strip() for line in input_data.readlines()]
 
-    # Get the alignment.
-    alignment = global_alignment(word1, word2, BLOSUM62(), 5)
+    # # Get the alignment.
+    # alignment = global_alignment(word1, word2, BLOSUM62(), 5)
 
-    # Print and save the answer.
-    print '\n'.join(alignment)
-    with open('output/Assignment_06E.txt', 'w') as output_data:
-        output_data.write('\n'.join(alignment))
+    # # Print and save the answer.
+    # print '\n'.join(alignment)
+    # with open('output/Assignment_06E.txt', 'w') as output_data:
+    #     output_data.write('\n'.join(alignment))
+
+    import cProfile
+    cProfile.run('global_alignment(word1, word2, BLOSUM62(), 5)')
