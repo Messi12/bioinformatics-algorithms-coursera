e36b53505623ad99eae00be4f86278b870d4bdb9
jschendel <Jeremy.R.Schendel@gmail.com>
2014-01-06 19:53:15 -0600

    Scoring Matrices (Updated)
    Changed to BLOSUM62 scoring matrix to use a dictionary instead of a
    Pandas dataframe to increase the speed of repeatedly returning items.
    Functionality remains the same.

--- scripts/scoring_matrices.py
+++ scripts/scoring_matrices.py
@@ -8,13 +8,14 @@ class BLOSUM62(object):
     def __init__(self):
         """Initialize the scoring matrix."""
         import os
-        import pandas as pd
-        # Convert the scoring matrix text file to a data frame.
-        self.scoring_matrix = pd.read_table(os.path.join(os.path.dirname(__file__), 'data/BLOSUM62.txt'), sep='  ')
+
+        with open(os.path.join(os.path.dirname(__file__), 'data/BLOSUM62.txt')) as input_data:
+            items = [line.strip().split() for line in input_data.readlines()]
+            self.scoring_matrix = {(item[0], item[1]):int(item[2]) for item in items}
 
     def __getitem__(self, pair):
         """Returns the score of the given pair of protein."""
-        return self.scoring_matrix[pair[0]][pair[1]]
+        return self.scoring_matrix[pair[0], pair[1]]
 
 
 class PAM250(object):
