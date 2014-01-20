8bfbcedade89768efaa7ea577c3306ed045afb47
jschendel <Jeremy.R.Schendel@gmail.com>
2014-01-20 14:54:54 -0600

    Suffix Tree Construction Problem (formatting update)
    Fixed some typos and formatting for cosmetic purposes, code still runs
    exactly the same.  Removed the edge generation and formatting from the
    main call and put it in its own function.

--- Assignment_09D.py
+++ Assignment_09D.py
@@ -9,31 +9,38 @@ Assignment #: 09
 Problem ID: D
 URL: https://stepic.org/Bioinformatics-Algorithms-2/Suffix-Trees-296/step/4
 '''
+from scripts import GeneralizedSuffixTree
+
+
+def suffix_tree_edges(word):
+    '''Returns the edge subsrings associated with the suffix tree for the given word.'''
+
+    # Most of the work is done by the generalized suffix tree script (see scripts folder).
+    gst = GeneralizedSuffixTree(word)
+
+    # Get a list of all edge substrings from the generalized suffix tree.
+    edges = [gst.edge_word(e) for e in gst.edges.values()]
+
+    # Return the edges in suffix tree format (i.e. want endings $0 to be $).
+    # Note: This is necessary because we're using a generalized suffix tree, which uses $0, $1, ..., $N
+    # as the out of alphabet suffixes in order to distinguish between word 0, word 1, ..., word N.
+    return [e[:-1] if '$' in e else e for e in edges]
 
 
 def main():
-    '''Solves problem Problem 9D.'''
-    from scripts import GeneralizedSuffixTree
+    '''Main call. Reads, runs, and saves problem specific data.'''
 
     # Read the input data.
     with open('data/stepic_9d.txt') as input_data:
         text = input_data.read().strip()
 
-    # Most of the workd is done by the generalized suffix tree script.
-    gst = GeneralizedSuffixTree(text)
-
-    # Get a list of all edges from the generalized suffix tree.
-    edges = [gst.edge_word(e) for e in gst.edges.values()]
-
-    # Put the edges in the proper format (i.e. want endings $0 to be $).
-    # Note: This is necessary because we're using a generalized suffix tree, which uses
-    # $X as the out of alphabet suffixes in order to distinguish between multiple words.
-    edges = [e[:-1] if '$' in e else e for e in edges]
+    # Get the edge substrings.
+    edge_substrings = suffix_tree_edges(text)
 
     # Print and save the answer.
-    print '\n'.join(edges)
+    print '\n'.join(edge_substrings)
     with open('output/Assignment_09D.txt', 'w') as output_data:
-        output_data.write('\n'.join(edges))
+        output_data.write('\n'.join(edge_substrings))
 
 if __name__ == '__main__':
     main()
